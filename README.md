3e63d5e172ba1ea857b6dac18041e697db429b86
Abdelrahman Ehab <abdu.ehab0@gmail.com>
2022-05-06 16:14:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cmake-Template Repo
+
+For those who are like me and just wanna yoink some ready-to-go clean project files.
---
+++ src/Main.cpp
@@ -0,0 +1,11 @@
+#include <Constants.h>
+
+void print(const char *message)
+{
+    std::cout << message << std::endl;
+}
+
+int main(int, char **)
+{
+    print("Hello, world!\n");
+}
